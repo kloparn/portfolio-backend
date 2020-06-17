755f6f09dfd65d955bd7bfe5dc807714ccd7b199
kloparn <adam97.live.com@live.com>
2020-06-17 18:46:36 +0200

    Added cors, as that will avoid some issues with REST requests

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
 const express = require("express");
 const { user, backend, frontend } = require("./scr/database/api");
 const app = express();
+const cors = require("cors");
 
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://adamh-portfolio.netlify.app"
-  );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(
